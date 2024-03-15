39f431a42ae8b76268fc2dddf0225fe1c00fb049
Abhijit Shendage <70395874+sabhi303@users.noreply.github.com>
2024-03-15 19:29:55 +0530

    Comments and latest files

--- generator.py
+++ generator.py
@@ -2,6 +2,7 @@ import configparser
 import numpy as np
 import csv
 
+
 # this will return all the read parameters in the dictionary format
 def parse_config(config_file) -> dict:
     # print('in parse config')
@@ -13,36 +14,28 @@ def parse_config(config_file) -> dict:
     for section in cp.sections():
         config[section] = dict(cp.items(section=section))
 
-    return config    
-
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-    
-    seed = int(lines[0].strip())
-    num_samples = int(lines[1].strip())
-    
-    distributions = []
-    for line in lines[2:]:
-        parts = line.strip().split()
-        dist_name = parts[0].lower()
-        dist_params = list(map(float, parts[1:]))
-        distributions.append((dist_name, dist_params))
-
-        return seed, num_samples, distributions
+    return config
 
 # convert all values to integer or floating of the dictionary
 # and convert keys to lowercase
 def convert_to_float(dictionary) -> dict:
-     int_dictionary = {}
-     for key, value in dictionary.items():
-        int_dictionary[key.lower()] = dict(map(lambda x: (x[0].lower(), float(x[1]) if '.' in x[1] else int(x[1])), value.items()))
+    int_dictionary = {}
+    for key, value in dictionary.items():
+        int_dictionary[key.lower()] = dict(
+            map(
+                lambda x: (x[0].lower(), float(x[1]) if "." in x[1] else int(x[1])),
+                value.items(),
+            )
+        )
 
-     return int_dictionary
+    return int_dictionary
+
 
 # Define the inverse transform sampling functions for Dagum and Skellam distributions
 def inverse_transform_dagum(alpha, beta, p, size=1):
     u = np.random.random(size)
     return (1 / beta) * ((1 - (1 - u) ** (1 / alpha)) ** (1 / p))
+
 
 def inverse_transform_skellam(mu1, mu2, size=1):
     x = np.zeros(size)
@@ -54,68 +47,64 @@ def inverse_transform_skellam(mu1, mu2, size=1):
         x[i] = s1 - s2
     return x
 
-def generate_data(parameters):
 
+def generate_data(parameters):
     # modify the key values to correct datatype and case format
     parameters = convert_to_float(parameters)
     # will seed the random data
     np.random.seed(parameters.get("rng").get("seed"))
-    del parameters["rng"] #as this is not required further
+    del parameters["rng"]  # as this is not required further
 
     generated_data = {}
 
     for distribution, dist_params in parameters.items():
         distributed_data = []
-        if distribution == 'gamma':
+        if distribution == "gamma":
             distributed_data = np.random.gamma(
-                    dist_params.get("shape1"),
-                    dist_params.get("shape2"),  
-                    dist_params.get("size")
+                dist_params.get("shape1"),
+                dist_params.get("shape2"),
+                dist_params.get("size"),
             )
-        elif distribution == 'exponential':
+        elif distribution == "exponential":
             distributed_data = np.random.exponential(
-                scale=dist_params.get("scale"),
-                size = dist_params.get("size")
+                scale=dist_params.get("scale"), size=dist_params.get("size")
             )
-        elif distribution == 'gaussian':
+        elif distribution == "gaussian":
             distributed_data = np.random.normal(
-                scale=dist_params.get("scale"),
-                size = dist_params.get("size")
+                scale=dist_params.get("scale"), size=dist_params.get("size")
             )
-        elif distribution == 'dag':
+        elif distribution == "dag":
             distributed_data = inverse_transform_dagum(
-                alpha = dist_params.get("alpha"), 
-                beta = dist_params.get("beta"), 
-                p = dist_params.get("p"), 
-                size= dist_params.get("size")
+                alpha=dist_params.get("alpha"),
+                beta=dist_params.get("beta"),
+                p=dist_params.get("p"),
+                size=dist_params.get("size"),
             )
-        elif distribution == 'geometric':
+        elif distribution == "geometric":
             distributed_data = np.random.geometric(
-                p = dist_params.get("p"), 
-                size=dist_params.get("size")
-                )
-            
-        elif distribution == 'poisson':
+                p=dist_params.get("p"), size=dist_params.get("size")
+            )
+
+        elif distribution == "poisson":
             distributed_data = np.random.poisson(
-                lam= dist_params.get("lam"), 
-                size=dist_params.get("size")
-                )
-        elif distribution == 'skellam':
+                lam=dist_params.get("lam"), size=dist_params.get("size")
+            )
+        elif distribution == "skellam":
             distributed_data = inverse_transform_skellam(
-                mu1 = dist_params.get("mu1"), 
-                mu2 = dist_params.get("mu2"), 
-                size= dist_params.get("size")
-                )
-            
+                mu1=dist_params.get("mu1"),
+                mu2=dist_params.get("mu2"),
+                size=dist_params.get("size"),
+            )
+
         generated_data[distribution] = distributed_data
-    
+
     return generated_data
 
 
 def write_to_csv(filename, generated_data) -> bool:
     print("Generated Data here: ", generated_data)
-    with open(filename, 'w', newline='') as file:
+    with open(filename, "w", newline="") as file:
         csv_writer = csv.writer(file)
         for distribution, data in generated_data.items():
-            csv_writer.writerow([f"{distribution} {data.shape[0]}"] + data.tolist())
+            csv_writer.writerow([f"{distribution}"] + data.tolist())
     return True
--- plotter.py
+++ plotter.py
@@ -1,32 +1,32 @@
 import matplotlib.pyplot as plt
 
 
-# plot CDF 
+# plot CDF
 def plot_CDF(data, title, plt):
     plt.subplot(1, 2, 1)
-    plt.hist(data, bins=100, density=True, cumulative=True, histtype='step', color='b')
+    plt.hist(data, bins=100, density=True, cumulative=True, histtype="step", color="b")
     plt.title(f"{title} CDF")
 
+
+# plt box plot
 def plot_box(data, title, plt):
     plt.subplot(1, 2, 2)
     plt.boxplot(data)
-    plt.title(f'{title} Boxplot')
+    plt.title(f"{title} Boxplot")
 
 
 def plot_graphs(filename, generated_data):
-    
     """Plot CDF and Boxplot and save as PDF files."""
     for distribution, data in generated_data.items():
         print("Data", data)
-        
+
         plt.figure(figsize=(10, 5))
-        
+
         plot_CDF(data, distribution, plt)
         plot_box(data, distribution, plt)
-        
+
         plt.tight_layout()
 
         # plt.show()
-        plt.savefig(f'{filename}-{distribution}-CDF-Box.pdf')
+        plt.savefig(f"{filename}-{distribution}-CDF-Box.pdf")
         plt.close()
-
